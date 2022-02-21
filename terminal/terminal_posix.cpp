d63fefe43f368ca37036ed2c92ed7bfa91da4744
yhsb2k <yhsb2k@outlook.com>
2022-02-21 20:35:56 +0200

    Fix Linux build

--- terminal/terminal_posix.cpp
+++ terminal/terminal_posix.cpp
@@ -11,9 +11,13 @@ terminal_posix::~terminal_posix()
     
 }
 
-std::vector<event> terminal_posix::read()
+std::vector<terminal::event> terminal_posix::read()
 {
+    std::vector<event> terminal_events;
     
+    // TODO
+    
+    return terminal_events;
 }
 
 void terminal_posix::write(char byte)
